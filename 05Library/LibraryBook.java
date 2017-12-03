59bc8d759f7f4f47d59e2ae4bd32d4d99e645f39
Anne Zhang <azhang14@stuy.edu>
2017-12-03 13:51:29 -0500

    everything works for library book...

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,15 @@
+abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+    public String getCallNumber(){
+	return callNumber;
+    }
+    public void setCallNumber(){
+	callNumber = c;
+    }
+    
+    
+    public LibraryBook(String a, String t, String i, String c){
+    }
+    
+
+}
