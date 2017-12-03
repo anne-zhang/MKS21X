a6b56a2d269a866c49596e4de92e272554715445
Anne Zhang <azhang14@stuy.edu>
2017-12-03 14:19:24 -0500

    compiles

--- 05Library/LibraryBook.java
+++ 05Library/LibraryBook.java
@@ -1,15 +1,34 @@
-abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+
     private String callNumber;
+
+    public LibraryBook(String author, String title, String ISBN, String callNumber){
+	super(author, title, ISBN);
+	this.callNumber = callNumber;
+    }
+
+    
+    
     public String getCallNumber(){
-	return callNumber;
+	return callNumber + "";
     }
-    public void setCallNumber(){
-	callNumber = c;
+
+    public void setCallNumber(String callNumber){
+	this.callNumber = callNumber;
     }
-    
-    
-    public LibraryBook(String a, String t, String i, String c){
-    }
+
+    abstract void checkout(String patron, String due);
     
 
+    abstract void returned();
+
+    abstract String circulationStatus();
+
+    public int compareTo(LibraryBook x){
+	return callNumber.compareTo(x.getCallNumber()) ;
+      }
+    
+    public String toString(){
+	return super.toString() + "\nCall Number: " + callNumber;
+    }
 }
