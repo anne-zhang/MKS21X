aaed7fcef9a14fe20fe322765d83ec882b8835a5
Anne Zhang <azhang14@stuy.edu>
2017-12-04 00:26:07 -0500

    compiles and works

---
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class SuperArrayIterator implements Iterator<String>{
+
+    private SuperArray ary;
+
+    private int start;
+
+    public SuperArrayIterator(SuperArray ary){
+	this.ary = ary;
+        start = 0;
+    }
+
+    public boolean hasNext(){
+        return ary.size() - start > 0;
+    }
+    
+    
+    public String next(){
+	if (hasNext()){
+	    start ++;
+	    return ary.get(start-1);
+	}
+	else {
+	    throw new NoSuchElementException();
+	}
+    }
+
+
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
+
+}
