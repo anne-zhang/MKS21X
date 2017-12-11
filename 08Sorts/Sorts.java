06c547aa61fbac92ad0088050786347982f85ee0
Anne Zhang <azhang14@stuy.edu>
2017-12-11 01:21:46 -0500

    tested with main bubble sort added and works

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -3,7 +3,14 @@ public class Sorts{
                                                                                     
     public static String name(){                                                    
         return "10.Zhang.Anne";                                                  
-    }                                                                               
+    }     
+    
+    public static void swap(int[] ary, int a, int b){
+    int c = ary[a];
+    ary[a] = ary[b];
+    ary[b] = c;
+  } 
+                                                                            
      public static void selectionSort(int[] data) {
 	for (int x = 0; x < data.length - 1; x++) {
 	    int minimum = x;
@@ -29,6 +36,63 @@ public class Sorts{
 	    }
 	}
     }
+    
+    public static void bubbleSort(int[] data){
+    for(int x = 0; x < data.length; x++) {
+    	for(int i = 0; i < data.length - x -1; i++){
+    		if(data[i] > data[i+1]){
+    		swap(data, i, i+1);
+    		}
+    	}
+    }
                                                                             
                                                                         
-}                                    
+}
+ public static void main(String[]artie){
+    long startTime = System.currentTimeMillis();
+    int[] randish = new int[1000];
+    //int[] randish = new int[0];
+    //int[] randish = new int[1];
+    //int[] randish = new int[2];
+    //int[] randish = new int[3];
+    //int[] randish = new int[100000];
+
+    for(int i = 0 ; i < randish.length; i++){ //PURELY RANDOM
+      randish[i] = (int)(Math.random()*100);
+    }
+
+    //for(int i = 0 ; i < randish.length; i++){ //ALL THE SAME NUMBER OR YOU CAN JUST RUN CODE WITHOUT ANY FOR LOOPS
+    //  randish[i] = 1;
+    //}
+
+    //for(int i = 0 ; i < randish.length; i++){ //BIGGEST TO SMALLEST
+    //  randish[i] = randish.length - i;
+    //}
+
+    //for(int i = 0 ; i < randish.length; i++){ // SMALLEST to BIGGEST
+    //  randish[i] = i;
+    //}
+
+    System.out.println(Arrays.toString(randish));
+
+    //Sorts Tests Here:
+
+    //System.out.println("selectionSort:");
+    //selectionSort(randish);
+
+    //System.out.println("insertionSort:");
+    //insertionSort(randish);
+
+    System.out.println("bubbleSort:");
+    bubbleSort(randish);
+
+    System.out.println(Arrays.toString(randish));
+
+    System.out.println("Runtime: " +
+                       (System.currentTimeMillis() - startTime) + "ms or " +
+                       (System.currentTimeMillis() - startTime)/1000.0 + "s"
+                       );
+
+  }
+
+}
